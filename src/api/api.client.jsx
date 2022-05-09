04fc6ac08091d6f062137fda649b1a58a4a2cf4d
Luigi Lesca <93204161+luigilesca@users.noreply.github.com>
2022-05-09 22:07:49 +0200

    Add files via upload

---
+++ src/api/api.client.jsx
@@ -0,0 +1,11 @@
+const baseURL = "https://api.spoonacular.com/recipes/";
+
+export default baseURL;
+
+// export const axiosPopular = axios.create({
+//   baseURL: ` https://api.spoonacular.com/recipes/random?apiKey=32079a0064ec446099187ebb5c0ea0c6&number=9&`,
+// });
+
+// export const axiosSearched = axios.create({
+//   baseURL: `https://api.spoonacular.com/recipes/complexSearch?apiKey=32079a0064ec446099187ebb5c0ea0c6&number=9&query=`,
+// });
---
+++ src/api/request.jsx
@@ -0,0 +1,8 @@
+const instance = {
+  fetchPopular: `/random?apiKey=32079a0064ec446099187ebb5c0ea0c6&number=9&tags=vegetarian`,
+  fetchInfo: `/information?apiKey=32079a0064ec446099187ebb5c0ea0c6`,
+  fetchSearch: `/complexSearch?apiKey=32079a0064ec446099187ebb5c0ea0c6&number=9&query=`,
+  dietVegetarian: `&diet=vegetarian`,
+};
+
+export default instance;
