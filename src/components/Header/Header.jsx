a15270b57a44400b901fd351dff5237453f31d87
Luigi Lesca <93204161+luigilesca@users.noreply.github.com>
2022-05-09 22:11:05 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,16 +5,24 @@ import { NavLink } from "react-router-dom";
 function Header() {
   return (
     <div className={styles.header}>
-      <NavLink to={"/"}>
-        <h4 className={styles.home}>Home</h4>
-      </NavLink>
+      <div className={styles.header_logo}>
+        <NavLink to={"/"}>
+          <h1 className={styles.logo}>Veg - Recipe</h1>
+        </NavLink>
+      </div>
 
-      <NavLink to={"/favourite/"}>
-        <div className={styles.link}>
-          <AiFillHeart className={styles.icon} />
-          <h4 className={styles.favourite}>Favourites</h4>
-        </div>
-      </NavLink>
+      <div className={styles.header_link}>
+        <NavLink to={"/"}>
+          <h4 className={styles.home}>Home</h4>
+        </NavLink>
+
+        <NavLink to={"/favourite/"}>
+          <div className={styles.link}>
+            <AiFillHeart className={styles.icon} />
+            <h4 className={styles.favourite}>Favourites</h4>
+          </div>
+        </NavLink>
+      </div>
     </div>
   );
 }
